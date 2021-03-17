edf21290bd1c0d11689c4c81e154fb2f5a8f287b
markus-posch <markus.posch1@stud.fh-campuswien.ac.at>
2021-03-17 21:15:16 +0100

    Added Implementation of Function gradingStudents for failed students

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result {
     public static List<Integer> gradingStudents(List<Integer> grades) {
-
+        List<Integer> gradesrounded = new ArrayList<>();
+        for(Integer grade : grades) {
+            if (grade < 38) {
+                gradesrounded.add(grade);
+            }
+        }
+        return gradesrounded;
     }
 }
