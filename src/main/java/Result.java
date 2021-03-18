2a6d0a0bb6fefe857d9e1558cc74dbee33c3a069
markus-posch <markus.posch1@stud.fh-campuswien.ac.at>
2021-03-18 10:40:28 +0100

    After talk with my colleagues recognized missunderstanding of the excercise Added new Test Methods for StudentNumber Changed function gradingStudents

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -4,25 +4,34 @@ import java.util.List;
 public class Result {
     public static List<Integer> gradingStudents(List<Integer> grades) {
         List<Integer> gradesrounded = new ArrayList<>();
-        for(Integer grade : grades) {
-            if (grade < 38) {
-                gradesrounded.add(grade);
-            }
-            else if ((grade%5) == 0){
-                gradesrounded.add(grade);
-            }
-            else if (grade % 5 == 3){
-                int newgrade = grade + 2;
-                gradesrounded.add(newgrade);
-            }
-            else if (grade % 5 == 4){
-                int newgrade = grade + 1;
-                gradesrounded.add(newgrade);
-            }
-            else {
-                gradesrounded.add(grade);
+        if(grades.get(0) >= 1 && grades.get(0) <= 60 && grades.size() > 1){
+            int i = 1;
+            while(i <= grades.get(0)){
+                if(grades.get(i) >= 0 && grades.get(i) <=100) {
+                    if (grades.get(i) < 38) {
+                        gradesrounded.add(grades.get(i));
+                    } else if ((grades.get(i) % 5) == 0) {
+                        gradesrounded.add(grades.get(i));
+                    } else if (grades.get(i) % 5 == 3) {
+                        int newgrade = grades.get(i) + 2;
+                        gradesrounded.add(newgrade);
+                    } else if (grades.get(i) % 5 == 4) {
+                        int newgrade = grades.get(i) + 1;
+                        gradesrounded.add(newgrade);
+                    } else {
+                        gradesrounded.add(grades.get(i));
+                    }
+                    i++;
+                }
+                else{
+                    System.out.println("Grade not in 0 to 100");
+                }
             }
         }
+        else{
+            System.out.println("Number of Students incorrect");
+        }
+
 
         return gradesrounded;
 
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,29 +16,41 @@ public class ResultTest {
     @BeforeEach
     void setUp(){}
 
+
     @Test
     @DisplayName("Method gradingStudents: Grades below 38")
     void testGradingsStudents_failed(){
-        List<Integer> actual = Result.gradingStudents(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31, 32, 33, 34, 35, 36, 37));
+        List<Integer> actual = Result.gradingStudents(Arrays.asList(18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31, 32, 33, 34, 35, 36, 37));
         List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31, 32, 33, 34, 35, 36, 37);
-
         assertEquals(expected, actual,"Something went wrong with grades below 38");
     }
 
     @Test
     @DisplayName("Method gradingStudents: Grades should round up")
     void testGradingsStudents_roundup(){
-        List<Integer> actual = Result.gradingStudents(Arrays.asList(38,43,48,58,63,73,89,99,59,41));
+        List<Integer> actual = Result.gradingStudents(Arrays.asList(10,38,43,48,58,63,73,89,99,59,41));
         List<Integer> expected = Arrays.asList(40,45,50,60,65,75,90,100,60,41);
-
         assertEquals(expected, actual,"Grades didn't round up correctly");
     }
     @Test
     @DisplayName("Method gradingStudents: Grades should stay same")
     void testGradingsStudents_staysame(){
-        List<Integer> actual = Result.gradingStudents(Arrays.asList(45,55,65,75,85,95));
+        List<Integer> actual = Result.gradingStudents(Arrays.asList(6,45,55,65,75,85,95));
         List<Integer> expected = Arrays.asList(45,55,65,75,85,95);
-
         assertEquals(expected, actual,"Grades should stay the same");
+    }
+    @Test
+    @DisplayName("Method gradingStudents: Students == 0")
+    void testGradingsStudents_numberStudents(){
+        List<Integer> actual = Result.gradingStudents(Collections.singletonList(0));
+        List<Integer> expected = Collections.emptyList();
+        assertEquals(expected, actual,"Something went wrong with zero Students");
+    }
+    @Test
+    @DisplayName("Method gradingStudents: Students but no grades")
+    void testGradingsStudents_numberStudents2(){
+        List<Integer> actual = Result.gradingStudents(Collections.singletonList(17));
+        List<Integer> expected = Collections.emptyList();
+        assertEquals(expected, actual,"There should be no grades in the List");
     }
 }
